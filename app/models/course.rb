77139202c3cfd695a8d5ac287f2509cb74e65056
Caleb Stein <caleb@calebstein.net>
2023-09-12 16:36:29 -0700

    added image type validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ApplicationRecord
+  validate :acceptable_image
   validates :image, presence: :true
   validates :title, presence: :true
   validates :description, presence: :true
@@ -7,4 +8,9 @@ class Course < ApplicationRecord
   has_one_attached :image
 
   default_scope { order(id: :desc) }
+
+  def acceptable_image
+    acceptable_types = ["image/jpeg", "image/png"]
+    errors.add(:image, "must be JPEG or PNG") unless acceptable_types.include?(image.content_type)
+  end
 end
