ebc8f236f55d6b4919a1ce50d84a2bf056840853
Caleb Stein <caleb@calebstein.net>
2023-12-07 00:08:04 -0800

    added page change animations

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import Turn from "@domchristie/turn"
 
 const application = Application.start()
+Turn.start()
 
 // Configure Stimulus development experience
 application.debug = false
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "@domchristie/turn", to: "https://ga.jspm.io/npm:@domchristie/turn@3.1.1/dist/turn.min.js"
